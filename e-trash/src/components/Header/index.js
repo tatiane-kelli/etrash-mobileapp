803a943feb2cecb535a0de8e43bffc382d4dd85e
Tatiane Kelli <tatianekelli.dev@gmail.com>
2022-04-10 12:48:57 -0300

    created: Header component

--- e-trash/src/components/Header/index.js
+++ e-trash/src/components/Header/index.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 const Header = () => {  
   return(
     <View style={styles.container}>
-      <Text>Funciona</Text>
+      <Image 
+        source={require('../../../assets/logo.png')}
+      />
+      <BorderlessButton>
+        <SimpleLineIcons name="menu" size={24} color="black" />
+      </BorderlessButton>
     </View>
   );
 }
@@ -17,6 +24,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center'
+  },
+  logoStyle: {
+    width: 55,
+    height: 24
   }
 })
 
